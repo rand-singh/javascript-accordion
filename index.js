cbd08c6a6d75f41d3b9a67316012bb7c902f83e9
Randeep Singh <rand-singh@users.noreply.github.com>
2017-07-15 17:05:07 +0100

    Added feature to close li's when another one is opened

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const questions = accordion.querySelectorAll('.question');
 
 function toggleAccordion() {
 	const thisItem = this.parentNode;
-	thisItem.classList.toggle('open');
+
+	items.forEach(item => {
+		if(thisItem == item){
+			thisItem.classList.toggle('open');			
+			return;
+		}
+
+		item.classList.remove('open');
+	});
 }
 
 questions.forEach(question => question.addEventListener('click', toggleAccordion));
