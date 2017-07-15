31203e92c043698a379c3da0c928ce598c4bd41f
Randeep Singh <rand-singh@users.noreply.github.com>
2017-07-15 15:39:31 +0100

    Added toggle functionality

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const items 	= accordion.querySelectorAll('li');
 const questions = accordion.querySelectorAll('.question');
 
 function toggleAccordion() {
-	console.log(this);
+	const thisItem = this.parentNode;
+	thisItem.classList.toggle('open');
 }
 
 questions.forEach(question => question.addEventListener('click', toggleAccordion));
